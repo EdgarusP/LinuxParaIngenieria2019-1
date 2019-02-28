0088ac711073c022d4ef001637470944abd4bc75
lujarocha <lujarocha@javerianacali.edu.co>
2019-02-28 16:01:13 -0500

    Nueva versión 4
    Funciona cambiar contraseña, solo falta transferencia entre cuentas.

--- practica4/cambiar.sh
+++ practica4/cambiar.sh
@@ -1,1 +1,25 @@
 clear
+cd bancos/cuentas/
+echo "-- R E S T A U R A C I Ó N --"
+echo "-----------------------------"
+echo "Contraseña Actual"
+read actualPass
+pass=`sed -n '2 p' $usuario.txt`
+if [ "$actualPass" = "$pass" ]; then
+	echo "Contraseña Nueva"
+	read newpass
+	tipo=`sed -n '3 p' $usuario.txt`
+	cuenta=`sed -n '4 p' $usuario.txt`
+	saldo=`sed -n '5 p' $usuario.txt`
+	echo "$usuario" > $usuario.txt
+	echo "$newpass" >> $usuario.txt
+	echo "$tipo" >> $usuario.txt
+	echo "$cuenta" >> $usuario.txt
+	echo "$saldo" >> $usuario.txt
+	echo "activo" >> $usuario.txt
+	echo "Contraseña actualizada con éxito."
+	sleep 3
+else
+	echo "Verifique su contraseña."
+	sleep 3
+fi
--- practica4/eliminar.sh
+++ practica4/eliminar.sh
@@ -7,13 +7,6 @@ echo "ingrese su contraseña: "
 read pwd
 
 cd bancos/cuentas
-fichero=$user.txt
-if  [ -f $fichero ]
-then 
-        tail -n 1 "$fichero" | wc -c | xargs -I {} truncate "$fichero" -s -{}
-        echo "inactivo" >> $fichero
-
-else
-        echo "asegurese de que la informacion ingresada esta bien escrita o de lo contrario proceda a registrar un usuario"
-
-fi
+tail -n 1 "$fichero" | wc -c | xargs -I {} truncate "$fichero" -s -{}
+echo "inactivo" >> $fichero
+cd ../../
